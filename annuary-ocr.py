caaf2c9dfcfa64d3c66e3ee68de1cf1bc6543149
Diego Montesinos <chesp.gommont@gmail.com>
2018-04-08 17:37:16 -0500

    Avance con cosas que no me acuerdo.

--- annuary-ocr.py
+++ annuary-ocr.py
@@ -1,24 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import cv2
 import numpy as np
 import pytesseract
+import io
 
-def binary_image (image_src):
+def binarize_image(image_src):
 
   gray = cv2.cvtColor(image_src, cv2.COLOR_BGR2GRAY)
   ret, binary = cv2.threshold(gray, 70, 255, cv2.THRESH_BINARY_INV)
 
-  kernel = np.ones((2, 2), np.uint8)
-  binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
-
   return binary
-
 
 def find_columns(binary_image):
 
-  kernel = np.ones((30, 10), np.uint8)
-  img_dilation = cv2.dilate(binary_image, kernel, iterations=1)
-  
-  im2, contours, hierarchy = cv2.findContours(img_dilation.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  kernel_closing = np.ones((13, 13), np.uint8)
+  image_closing = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, kernel_closing)
+
+  kernel_dilation = np.ones((30, 7), np.uint8)
+  image_dilation = cv2.dilate(image_closing, kernel_dilation, iterations=1)
+
+  im2, contours, hierarchy = cv2.findContours(image_dilation.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
   columns = []
   for contour in contours:
@@ -28,18 +31,22 @@ def find_columns(binary_image):
 
     if (area > 4000) and is_valid_column(x, y, w, h):
       columns.append((x, y, w, h))
-  
+
   return columns
 
 def find_rows(binary_image):
 
-  kernel = np.ones((1, 80), np.uint8)
-  img_dilation = cv2.dilate(binary_image, kernel, iterations=1)
+  kernel_dilation = np.ones((1, 10), np.uint8)
+  image_dilation = cv2.dilate(binary_image, kernel_dilation, iterations=1)
 
-  #cv2.imshow('dilation', img_dilation)
-  #cv2.waitKey(0)
+  #cv2.imwrite('output/dilation-rows.png', image_dilation)
 
-  im2, contours, hierarchy = cv2.findContours(img_dilation.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  kernel_closing = np.ones((1, 66), np.uint8)
+  image_closing = cv2.morphologyEx(image_dilation, cv2.MORPH_CLOSE, kernel_closing)
+
+  #cv2.imwrite('output/closing.png', image_closing)
+
+  im2, contours, hierarchy = cv2.findContours(image_closing.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
   rows = []
   for contour in contours:
@@ -47,43 +54,11 @@ def find_rows(binary_image):
     area = cv2.contourArea(contour)
     x, y, w, h = cv2.boundingRect(contour)
 
-    if (area > 1000):
+    if (area > 1000) and (h < 30):
       rows.append((x, y, w, h))
 
   return rows
 
-def clean_columns(columns):
-
-  cleaned = []
-
-  for column_a in columns:
-    inside = False
-
-    for column_b in columns:
-      if column_a != column_b:
-        inside = is_inside(column_a, column_b) or inside
-    
-    if inside:
-      cleaned.append(column_a)
-  
-  return cleaned
-
-def clean_rows(rows, columns):
-
-  clean = []
-
-  for i, row in enumerate(rows):
-    intersections = []
-
-    for j, column in enumerate(columns):
-      if are_instersected(row, column):
-        intersections.append(column)
-    
-    if len(intersections) > 0:
-      clean.append(row)
-  
-  return clean
-    
 def is_valid_column(x, y, w, h):
   ratio = w / h
 
@@ -95,50 +70,55 @@ def draw_boxes(boxes, stroke, image):
     x, y, w, h = box
     cv2.rectangle(image, (x, y), (x + w, y + h), stroke, 1)
 
-def is_inside(box_a, box_b):
-  x_a, y_a, w_a, h_a = box_a
-  x_b, y_b, w_b, h_b = box_b
+def get_registers(image_url):
 
-  inside_x = (x_b <= x_a) and ((x_b + w_b) >= (x_a + w_a))
-  inside_y = (y_b <= y_a) and ((y_b + h_b) >= (y_a + h_a))
+  # Load and crop image
+  image_src = cv2.imread(image_url)
+  image_src = image_src[0:3200, 0:2550]
 
-  return inside_x and inside_y
+  cv2.imwrite('source.png', image_src)
 
-def are_instersected(box_a, box_b):
-  x_a, y_a, w_a, h_a = box_a
-  x_b, y_b, w_b, h_b = box_b
+  binary_image = binarize_image(image_src)
 
-  test_axis_x = (abs(x_a - x_b) * 2) <= (w_a + w_b)
-  test_axis_y = (abs(y_a - y_b) * 2) <= (h_a + h_b)
+  cv2.imwrite('binary.png', binary_image)
+  
+  height, width = binary_image.shape[:2]
+  M = cv2.getRotationMatrix2D((width/2,height/2), 1.1, 1)
+  binary_image = cv2.warpAffine(binary_image, M, (width,height))
 
-  return test_axis_x and test_axis_y
+  cv2.imwrite('binary-rotated.png', binary_image)
+
+  # Get columns and rows
+  columns = find_columns(binary_image)
+  rows = find_rows(binary_image)
+
+  draw_boxes(columns, (0, 0, 255), image_src)
+  cv2.imwrite('output/columns.png', image_src)
+
+  draw_boxes(rows, (255, 0, 0), image_src)
+  cv2.imwrite('output/rows.png', image_src)
+
+  f = open('output.txt', 'wb')
+  for i, row in enumerate(rows):
+    x, y, w, h = row
+    
+    roi = binary_image[y:y+h, x:x+w]
+
+    #cv2.imwrite('output/row' + str(i) + '.png', roi)
+
+    text = pytesseract.image_to_string(roi)
+    f.write(text.encode('utf-8'))
+    f.write('\n'.encode('utf-8'))
+
+    print('readed ' + str(i))
+  
+  f.close()
 
 # Main script
-
 def main():
-  for i in range(1, 2):
-    image_src = cv2.imread('images/annuary/BaseGuerra000' + str(i) + '.jpg')
-    image_src = image_src[0:3200, 0:2550]
+  pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 
-    binary = binary_image(image_src)
+  registers = get_registers('images/annuary/BaseGuerra0009.jpg')
 
-    columns = find_columns(binary)
-    #columns = clean_columns(columns)
-
-    if len(columns) != 18:
-      print('En la ' + str(i) + ' no hay todas las columnas')
-      print(len(columns))
-
-    rows = find_rows(binary)
-    #rows = clean_rows(rows, columns)
-
-    draw_boxes(columns, (0, 0, 255), image_src)
-    draw_boxes(rows, (255, 0, 0), image_src)
-
-  #cv2.imshow('result', image_src)
-  cv2.imwrite('output' + str(i) + '.png', image_src)
-  #cv2.waitKey(0)
-
-  print('Done ' + str(i))
-
-main()
+if __name__ == '__main__':
+  main()
