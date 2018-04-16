6ce432d45a2c3467b04e764897584f5938327dc9
Diego Montesinos <chesp.gommont@gmail.com>
2018-04-16 17:03:20 -0500

    Add args and make process clear of annuary read

--- annuary-ocr.py
+++ annuary-ocr.py
@@ -1,10 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pytesseract
+import argparse
 import cv2
 import numpy as np
-import pytesseract
-import io
+import re
+
+# Constants
+IMAGE_SCALE = 0.35
+
+ONLY_UPPERCASE_LETTERS = re.compile(u'^[A-Z]*$')
+ONLY_NUMBERS           = re.compile(u'^[0-9]*$')
+ONLY_UPPERCASE_DOTS    = re.compile(u'^[A-Z. \(\)]*$')
+
+def show_scaled_image(title, img, scale, wait=True):
+
+  height, width = img.shape[:2]
+  resized = cv2.resize(img, (int(width * scale), int(height * scale)))
+
+  cv2.imshow(title, resized)
+
+  if wait:
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 def binarize_image(image_src):
 
@@ -13,112 +32,163 @@ def binarize_image(image_src):
 
   return binary
 
-def find_columns(binary_image):
+def find_rows(binary_image, args):
 
-  kernel_closing = np.ones((13, 13), np.uint8)
-  image_closing = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, kernel_closing)
-
-  kernel_dilation = np.ones((30, 7), np.uint8)
-  image_dilation = cv2.dilate(image_closing, kernel_dilation, iterations=1)
-
-  im2, contours, hierarchy = cv2.findContours(image_dilation.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-  columns = []
-  for contour in contours:
-
-    area = cv2.contourArea(contour)
-    x, y, w, h = cv2.boundingRect(contour)
-
-    if (area > 4000) and is_valid_column(x, y, w, h):
-      columns.append((x, y, w, h))
-
-  return columns
-
-def find_rows(binary_image):
-
-  kernel_dilation = np.ones((1, 10), np.uint8)
+  # Dilate image
+  kernel_dilation = np.ones((1, 15), np.uint8)
   image_dilation = cv2.dilate(binary_image, kernel_dilation, iterations=1)
 
-  #cv2.imwrite('output/dilation-rows.png', image_dilation)
-
-  kernel_closing = np.ones((1, 66), np.uint8)
+  if args.debug:
+    show_scaled_image('rows dilation', image_dilation, IMAGE_SCALE)
+  
+  # Close image
+  kernel_closing = np.ones((2, 95), np.uint8)
   image_closing = cv2.morphologyEx(image_dilation, cv2.MORPH_CLOSE, kernel_closing)
 
-  #cv2.imwrite('output/closing.png', image_closing)
+  if args.debug:
+    show_scaled_image('rows closing', image_closing, IMAGE_SCALE)
+  
+  # Find contours
+  im2, contours, hierarchy = cv2.findContours(image_closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-  im2, contours, hierarchy = cv2.findContours(image_closing.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+  if args.debug:
+    contours_img = draw_contours(binary_image, contours)
+    show_scaled_image('contours rows', contours_img, IMAGE_SCALE)
+  
+  # Create rows
   rows = []
   for contour in contours:
+    row = cv2.boundingRect(contour)
 
-    area = cv2.contourArea(contour)
-    x, y, w, h = cv2.boundingRect(contour)
-
-    if (area > 1000) and (h < 30):
-      rows.append((x, y, w, h))
+    if (is_valid_row(row)):
+      rows.append(row)
+  
+  print('Finded ' + str(len(rows)) + ' rows.')
 
   return rows
 
-def is_valid_column(x, y, w, h):
-  ratio = w / h
+def is_valid_row(row):
+  w = row[2]
+  h = row[3]
 
-  return (ratio < 1.0) and (w > 30)
+  area = w * h
+  return (area > 20000) and (h < 60)
 
-def draw_boxes(boxes, stroke, image):
+def draw_contours(binary_image, contours):
+  channel = cv2.split(binary_image)[0]
 
+  contours_img = cv2.merge((channel, channel, channel))
+  cv2.drawContours(contours_img, contours, -1, (0, 255, 0), 2)
+
+  return contours_img
+
+def draw_boxes(binary_image, boxes, stroke):
+  channel = cv2.split(binary_image)[0]
+
+  boxes_img = cv2.merge((channel, channel, channel))
   for box in boxes:
     x, y, w, h = box
-    cv2.rectangle(image, (x, y), (x + w, y + h), stroke, 1)
-
-def get_registers(image_url):
-
-  # Load and crop image
-  image_src = cv2.imread(image_url)
-  image_src = image_src[0:3200, 0:2550]
-
-  cv2.imwrite('source.png', image_src)
-
-  binary_image = binarize_image(image_src)
-
-  cv2.imwrite('binary.png', binary_image)
+    cv2.rectangle(boxes_img, (x, y), (x + w, y + h), stroke, 1)
   
-  height, width = binary_image.shape[:2]
-  M = cv2.getRotationMatrix2D((width/2,height/2), 1.1, 1)
-  binary_image = cv2.warpAffine(binary_image, M, (width,height))
+  return boxes_img
 
-  cv2.imwrite('binary-rotated.png', binary_image)
+def process_rows(binary_image, rows, args):
+  print('Process rows and dump into ' + args.output)
 
-  # Get columns and rows
-  columns = find_columns(binary_image)
-  rows = find_rows(binary_image)
+  bad_count = 0
 
-  draw_boxes(columns, (0, 0, 255), image_src)
-  cv2.imwrite('output/columns.png', image_src)
-
-  draw_boxes(rows, (255, 0, 0), image_src)
-  cv2.imwrite('output/rows.png', image_src)
-
-  f = open('output.txt', 'wb')
   for i, row in enumerate(rows):
     x, y, w, h = row
     
     roi = binary_image[y:y+h, x:x+w]
 
-    #cv2.imwrite('output/row' + str(i) + '.png', roi)
+    if args.debug:
+      cv2.imwrite('output/row' + str(i) + '.png', roi)
 
-    text = pytesseract.image_to_string(roi)
-    f.write(text.encode('utf-8'))
-    f.write('\n'.encode('utf-8'))
+    readed = pytesseract.image_to_string(roi)
+    register_str = readed.encode('utf-8')
 
-    print('readed ' + str(i))
+    try:
+      register = parse_register_str(register_str)
+      #print register
+    except Exception as e:
+      #user_input = input()
+      print e
+      
+      bad_count += 1
   
-  f.close()
+  print('Errors in: ' + str(bad_count) + ' registers of: ' + str(len(rows)))
+
+def parse_register_str(register_str):
+  tokens = register_str.split(' ')
+
+  if '\n' in register_str:
+    raise Exception('Register contains breakline, should be a line')
+    print('Here!!!!')
+
+  if len(tokens) < 3:
+    raise Exception('Insuficient tokens at register: ' + register_str)
+  
+  register_id = get_register_id(tokens)
+
+  index_id = len(tokens[0]) + len(tokens[1]) + 1
+  name = register_str[index_id:].strip()
+  name = name.replace(',', '.')
+
+  if ONLY_UPPERCASE_DOTS.match(name) == None:
+    raise Exception('Invalid name: ' + name)
+
+  return { 'id' : register_id, 'name' : name }
+
+def get_register_id(tokens):
+
+  # Get and validate id
+  letters_id = tokens[0]
+  numbers_id = tokens[1]
+
+  if (ONLY_UPPERCASE_LETTERS.match(letters_id) == None) or (len(letters_id) < 2):
+    raise Exception('Bad letters id: ' + letters_id)
+  
+  if ONLY_NUMBERS.match(numbers_id) == None:
+    raise Exception('Bad numbers id: ' + numbers_id)
+  
+  return (letters_id, int(numbers_id))
+
+def process_image(args):
+  print('Processing file ' + args.input + '...')
+
+  # Read image source
+  print('Readed image source.')
+  image_src = cv2.imread(args.input)
+  if args.debug:
+    show_scaled_image('source', image_src, IMAGE_SCALE)
+
+  # Get binary image
+  binary_image = binarize_image(image_src)
+  if args.debug:
+    show_scaled_image('binary', binary_image, IMAGE_SCALE)
+  
+  # Get rows
+  rows = find_rows(binary_image, args)
+  if args.debug:
+    boxes_img = draw_boxes(binary_image, rows, (0, 255, 0))
+    show_scaled_image('rows', boxes_img, IMAGE_SCALE)
+  
+  # Process rows
+  process_rows(binary_image, rows, args)
 
 # Main script
 def main():
-  pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 
-  registers = get_registers('images/annuary/BaseGuerra0009.jpg')
+  # Config parser
+  parser = argparse.ArgumentParser(description='Digitalization of Annuary-part from Francois-Xavier Guerra database.')
+  parser.add_argument('-i', '--input', help='Input image file', required=True)
+  parser.add_argument('-o', '--output', help='Output text file', default='output.txt')
+  parser.add_argument('-d', '--debug', help='Debug parameter', action='store_true')
+
+  args = parser.parse_args()
+  process_image(args)
 
 if __name__ == '__main__':
+  pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
   main()
