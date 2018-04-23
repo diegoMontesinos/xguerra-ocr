8fefe45b35bc00713badf7a94fc638ccb48d8d1c
Diego Montesinos <chesp.gommont@gmail.com>
2018-04-23 16:52:00 -0500

    Rename row_detector.py

--- src/rows_detector.py
+++ src/rows_detector.py
@@ -14,27 +14,29 @@ def find_rows(binary_image, args):
     show_scaled_image('rows dilation', image_dilation, 0.35)
   
   # Close image
-  kernel_closing = np.ones((2, 95), np.uint8)
+  kernel_closing = np.ones((1, 95), np.uint8)
   image_closing = cv2.morphologyEx(image_dilation, cv2.MORPH_CLOSE, kernel_closing)
 
   if args.debug:
-    show_scaled_image('rows closing', image_closing, 0.35)
+    show_scaled_image('rows closing', image_closing, 0.6)
   
   # Find contours
   im2, contours, hierarchy = cv2.findContours(image_closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
   if args.debug:
     contours_img = draw_contours(binary_image, contours)
-    show_scaled_image('contours rows', contours_img, 0.35)
+    show_scaled_image('contours rows', contours_img, 0.6)
   
-  # Create rows
+  # Create rows and sort them
   rows = []
   for contour in contours:
     row = cv2.boundingRect(contour)
 
     if (is_valid_row(row)):
       rows.append(row)
-
+  
+  rows.sort(key=lambda row: row[1])
+  rows.sort(key=lambda row: row[0])
   return rows
 
 def is_valid_row(row):
@@ -42,4 +44,4 @@ def is_valid_row(row):
   h = row[3]
 
   area = w * h
-  return (area > 20000) and (h < 60)
+  return (area > 12000) and (h < 60)
