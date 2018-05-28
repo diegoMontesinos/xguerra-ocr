ee03a4eb59c29386fc5b7bad6fe5ff2c14a17ccf
Diego Montesinos <chesp.gommont@gmail.com>
2018-05-27 20:42:03 -0500

    Choose and fix the register.

--- diary_ocr.py
+++ diary_ocr.py
@@ -7,10 +7,11 @@ import argparse
 import cv2
 import numpy as np
 import os
-from src import AnnuaryData, crop_roi, show_scaled_image, fix_image_rotation, \
-                binarize_image, find_columns_on_diary, find_blocks_on_diary_col, \
-                parse_annuary_register_str, AnnuaryParsingException, get_tesseract_cmd, \
-                find_diary_content_modules
+from src import AnnuaryData, DiaryData, crop_roi, show_scaled_image, \
+                fix_image_rotation, binarize_image, find_columns_on_diary, \
+                find_blocks_on_diary_col, parse_annuary_register_str, \
+                find_diary_content_modules, AnnuaryParsingException, \
+                get_tesseract_cmd, parse_num_id_only
 
 PAGE_ROI = (100, 200, 3400, 4650)
 
@@ -18,18 +19,19 @@ class DiaryOCR:
 
   def __init__(self, args):
     self.annuary_data = AnnuaryData(args.annuary)
-    #self.diary_data = DiaryData(args.diary)
+    self.diary_data = DiaryData(args.output)
 
     self.input_path = args.input
     self.debug = args.debug
 
   def start(self):
-    print('Processing file ' + self.input_path + '...')
 
     if not os.path.exists(self.input_path):
-      print('Error on reading or file input dont exist. ( ∩ ︵ ∩ )')
+      print('\nError on reading or file input dont exist. ( ∩ ︵ ∩ )')
       return
-  
+
+    print('\nProcessing file ' + self.input_path + '...')
+
     # Read image source and crop it
     image_src = cv2.imread(self.input_path)
     image_src = crop_roi(image_src, PAGE_ROI)
@@ -42,19 +44,28 @@ class DiaryOCR:
     if self.debug:
       show_scaled_image('binary', binary_image, 0.4)
     
-    # Get columns
+    # Get columns and process
     cols = find_columns_on_diary(binary_image, self.debug)
+    print('Detected ' + str(len(cols)) + ' columns.')
+
+    col_num = 1
     for col in cols:
+      print('\nProcessing ' + str(col_num) + '/' + str(len(cols)) + ' column...')
+
       img_col = crop_roi(binary_image, col)
       self.process_col(img_col)
+
+      col_num += 1
   
   def process_col(self, img_col):
 
     if self.debug:
       show_scaled_image('col', img_col, 0.4)
-
-    # Find blocks from the col
+    
+    # Find blocks from the column and process
     blocks = find_blocks_on_diary_col(img_col, self.debug)
+    print('  Detected ' + str(len(blocks)) + ' blocks.')
+
     for block in blocks:
       self.process_block(img_col, block)
   
@@ -82,6 +93,10 @@ class DiaryOCR:
     bytes_readed = pytesseract.image_to_string(header_img, config=config_str)
     readed_str = bytes_readed.encode('utf-8')
 
+    return self.process_readed_str(header_img, readed_str)
+
+  def process_readed_str(self, header_img, readed_str):
+
     # Try parse header and catch errors
     try:
       readed_register = parse_annuary_register_str(readed_str)
@@ -92,20 +107,20 @@ class DiaryOCR:
       # Not registered
       if not annuary_register:
         self.annuary_data.add_register(readed_register)
-        print('Added register: ' + str(readed_register))
+        print('  ---\n  Added register: ' + str(readed_register))
         
         return readed_register
       
       # Registered but not equal
       elif not self.are_registers_equals(readed_register, annuary_register):
-        return self.choose_register(readed_register, annuary_register)
-      
+        return self.choose_register(header_img, readed_register, annuary_register)
+
       # Registered
       else:
         return readed_register
 
     except AnnuaryParsingException as exception:
-      return self.fix_annuary_register(exception)
+      return self.fix_annuary_register(header_img, readed_str, exception)
   
   def are_registers_equals(self, register_a, register_b):
     return (register_a['text_id'] == register_b['text_id']) and \
@@ -113,12 +128,59 @@ class DiaryOCR:
            (register_a['type'] == register_b['type']) and \
            (register_a['name'] == register_b['name'])
   
-  def choose_register(self, register_a, register_b):
-    print register_a
-    print register_b
+  def choose_register(self, header_img, register_a, register_b):
+
+    # Get option from user
+    print('  ---\n  Differences were found in registers.')
+    print('  1. ' + str(register_a))
+    print('  2. ' + str(register_b))
+
+    # Show image
+    cv2.imshow('Header', header_img)
+    cv2.waitKey(10)
+
+    user_input = raw_input('  Help me to choose one: ')
+    if user_input == '1':
+      choosed = register_a
+    else:
+      choosed = register_b
+    
+    # Close image and update in data
+    cv2.destroyAllWindows()
+    
+    self.annuary_data.update_register(choosed)
+
+    return choosed
+
+  def fix_annuary_register(self, header_img, readed_str, exception):
+
+    annuary_register = None
+
+    if AnnuaryParsingException.BAD_NUMERIC_ID != exception.error_code:
+      try:
+        num_id = parse_num_id_only(readed_str)
+        annuary_register = self.annuary_data.search_by_num_id(num_id)
+      except AnnuaryParsingException as exception:
+        annuary_register = None
+    
+    user_should_fix = not annuary_register
+    if user_should_fix:
+      return self.user_fix_error(header_img, exception)
+
+    return annuary_register
   
-  def fix_annuary_register(self, exception):
-    print exception
+  def user_fix_error(self, header_img, exception):
+    print('  ---\n  Please, help me to fix the following error (● ω ●):')
+    print('  ' + str(exception))
+
+    # Show image
+    cv2.imshow('Header', header_img)
+    cv2.waitKey(10)
+
+    user_input = raw_input('  Enter the fixed register: ')
+    cv2.destroyAllWindows()
+
+    return self.process_readed_str(header_img, user_input)
 
   def read_content(self, content_img):
 
@@ -182,7 +244,7 @@ def print_welcome_message():
   print('::                                                     ::')
   print(':: Lucía Granados (luciagranadosriveros@gmail.com)     ::')
   print(':: Diego Montesinos (diegomontesinos@ciencias.unam.mx) ::')
-  print(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n')
+  print(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
 
 # Main script
 def main():
@@ -190,6 +252,7 @@ def main():
   # Check tesseract installation
   tesseract_cmd = get_tesseract_cmd()
   if (not tesseract_cmd) or (tesseract_cmd == ''):
+    print 'Tesseract not installed'
     return
   
   pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
@@ -198,10 +261,11 @@ def main():
   parser = argparse.ArgumentParser(description='A digitalization of diary section from Francois-Xavier Guerra database.')
   parser.add_argument('-i', '--input', help='Input image file')
   parser.add_argument('-a', '--annuary', help='Annuary CSV file', default='csv/annuary.csv')
+  parser.add_argument('-o', '--output', help='Output CSV file', default='csv/diary.csv')
   parser.add_argument('-d', '--debug', help='Enable debug option', action='store_true')
 
   args = parser.parse_args()
-
+  
   if not args.input:
     print('error: argument -i/--input is required')
     return
